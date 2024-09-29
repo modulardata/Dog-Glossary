8f28843481b5f364fcc5e8da94b4f47f8b2534a5
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-09-29 13:02:26 +0200

    Stage 1/5 short solution

--- Dog Glossary/task/src/script.js
+++ Dog Glossary/task/src/script.js
@@ -1,20 +1,14 @@
-const content = document.querySelector("#content");
-const button = document.querySelector("#button-random-dog");
-
-const apiUrl = "https://dog.ceo/api/breeds/image/random";
-
-button.addEventListener("click", async () => {
-	try {
-    const response = await fetch(apiUrl);
-		console.log(response);
-    const data = await response.json();
-    const imgUrl = data.message;
-    content.innerHTML = `<img src="${imgUrl}" alt="Random Dog Image">`;
-  } catch (error) {
-    console.error(error);
-    content.innerHTML = "Error fetching random dog image.";
-  }
-})
+const divContent = document.getElementById('content');
+const buttonRandomDog = document.getElementById('button-random-dog');
+const img = document.getElementById('dog-image');
+const url = 'https://dog.ceo/api/breeds/image/random';
 
 
 
+buttonRandomDog.addEventListener('click', () => {
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			img.src = data.message;
+		});
+});
