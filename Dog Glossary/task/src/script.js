326e88e569b2114632f789c2cc23019cde085ad2
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-10-08 15:59:49 +0200

    Solution 1/5 async await

--- Dog Glossary/task/src/script.js
+++ Dog Glossary/task/src/script.js
@@ -3,12 +3,11 @@ const buttonRandomDog = document.getElementById('button-random-dog');
 const img = document.getElementById('dog-image');
 const url = 'https://dog.ceo/api/breeds/image/random';
 
-
-
-buttonRandomDog.addEventListener('click', () => {
-	fetch(url)
+buttonRandomDog.addEventListener('click', async () => {
+	await fetch(url)
 		.then(response => response.json())
 		.then(data => {
 			img.src = data.message;
-		});
+		})
+		.catch(error => console.error('Error fetching random dog image:', error.message));
 });
